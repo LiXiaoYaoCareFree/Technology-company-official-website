2b4177d256e4eb2e90e2e0e362112877c6aef9a0
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-20 20:46:30 +0800

    团队介绍模块实现

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from slide.models import Slide
+from team.models import Team
 
 # Create your views here.
 def index(request):
     # 获取slide表中的所有数据
     slides = Slide.objects.all()
-    return render(request, 'index.html', {'slides': slides})
+    # 获取team表中的所有数据
+    teams = Team.objects.all().order_by('rank')
+    return render(request, 'index.html', {
+        'slides': slides,
+        'team': teams,
+        })
+
+
