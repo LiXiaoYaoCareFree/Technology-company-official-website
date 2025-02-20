d58b0144a2effb346f2d34e12b3ef2cecb12fa7d
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-20 21:32:05 +0800

    文章管理模块实现

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from news.models import Category, News
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'category')
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)
