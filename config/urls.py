0fbf7276ecefc44c4f8b0d6cc66b76dff6cae91b
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-20 20:21:22 +0800

    幻灯片效果前端显示

--- config/urls.py
+++ config/urls.py
@@ -21,3 +21,9 @@ urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
 ]
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from slide.models import Slide
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # 获取slide表中的所有数据
+    slides = Slide.objects.all()
+    return render(request, 'index.html', {'slides': slides})
